ac22d241d667ecec9f86132cc789bc866c08ca91
Spring-ch <2350824600@qq.com>
2021-03-11 02:29:07 +0800

    deploy

---
+++ precache-manifest.61f1e6202e4cb15f90ac47838496e038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88c3c3a72e2ce3b6861",
+    "url": "/accountBook-webisite/css/app.a4efa160.css"
+  },
+  {
+    "revision": "a0b13bb9b7cbd2b7bee7ae5c1158d11d",
+    "url": "/accountBook-webisite/index.html"
+  },
+  {
+    "revision": "a88c3c3a72e2ce3b6861",
+    "url": "/accountBook-webisite/js/app.0e54b017.js"
+  },
+  {
+    "revision": "d724d713146e1a1d9205",
+    "url": "/accountBook-webisite/js/chunk-vendors.d05e877d.js"
+  },
+  {
+    "revision": "ccbf010baa24c10f570ae55b944ac1dd",
+    "url": "/accountBook-webisite/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accountBook-webisite/robots.txt"
+  }
+]);
